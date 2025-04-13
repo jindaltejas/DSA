a8f01f64ea640abed216c37bf59ea03ce371f4b3
jindaltejas <jindaltejas219@gmail.com>
2025-04-13 11:17:43 +0530

    Building tree from level order traversal

--- BinaryTrees/createBinaryTree.cpp
+++ BinaryTrees/createBinaryTree.cpp
@@ -16,6 +16,9 @@ class node {
 };
 
 void LevelOrderTraversal (node* root) {
+    if (root == NULL) {
+        return;
+    }
     queue<node*> q;
     q.push(root);
     q.push(NULL);
@@ -46,7 +49,7 @@ void LevelOrderTraversal (node* root) {
 
 // }
 
-void InOrderTraversal(node* root) {
+void InOrderTraversal (node* root) {
     if (root == NULL) {
         return;
     }
@@ -76,8 +79,41 @@ void PostOrderTraversal (node* root) {
     cout << root->data << " ";
 }
 
+// 1 3 5 7 11 17 -1 -1 -1 -1 -1 -1 -1
+void buildFromLevelOrder (node* &root) {
+    queue<node*> q;
+    cout << "Enter data for root: " << endl; 
+    int rootData;
+    cin >> rootData;
+    root = new node(rootData);
+    q.push(root);
+
+    while (!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        cout << "Enter data for left of: " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if (leftData != -1) {
+            temp->left = new node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter data for right of: " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if (rightData != -1) {
+            temp->right = new node(rightData);
+            q.push(temp->right);
+        }
+    }
+} 
+
 // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
-node* buildTree(node* root) {
+node* buildTree (node* root) {
     int data;
     cout << "Enter data for root:" << endl;
     cin >> data;
@@ -98,15 +134,17 @@ node* buildTree(node* root) {
 int main() {
     node* root = NULL;
     
-    root = buildTree(root);
-    LevelOrderTraversal(root);
-    InOrderTraversal(root);
-    cout << endl;
-    PreOrderTraversal(root);
-    cout << endl;
-    PostOrderTraversal(root);
-    cout << endl;
+    // root = buildTree(root);
+    // LevelOrderTraversal(root);
+    // InOrderTraversal(root);
+    // cout << endl;
+    // PreOrderTraversal(root);
+    // cout << endl;
+    // PostOrderTraversal(root);
+    // cout << endl;
     // ReverseLevelOrderTraversal(root);
     
+    buildFromLevelOrder(root);
+    LevelOrderTraversal(root);
     return 0;
 }
